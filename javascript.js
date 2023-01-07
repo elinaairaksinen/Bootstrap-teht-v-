64ab34a1ed231af72fbf8bd2082026400dbc27f0
elinaairaksinen <118376901+elinaairaksinen@users.noreply.github.com>
2023-01-07 20:26:35 +0200

    finished mainecoon.html

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function vaihasivua() {
+    location.href = "https://www.mainecoon.fi/";
+}
+
+function vaihasivua1(){
+    location.href = "https://fi.wikipedia.org/wiki/Maine_coon"
+}
+
+function vaihasivua2(){
+    location.href = "https://www.tiedonportailla.fi/kissat/mainecoon.htm"
+}
